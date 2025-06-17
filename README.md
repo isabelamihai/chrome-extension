81c48c1fa1d70d31a2acf9b46a1769ebfcc4179f
Isabela Elena Mihai <isabelaelenamihai@gmail.com>
2025-06-17 20:36:07 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ This app is a chrome extension made for saving leads, websites etc.
 <br/>
 <br/>
 When entering manually a lead/website etc click on "SAVE INPUT" button. <br/>
-When saving directly the current tab of the browser click "SAVE TAB".  <br/>
+When saving directly the current tab of the browser click "SAVE TAB". <br/>
 When deleting all lead/websites double click on "DELETE ALL". <br/>
 Be aware that the extension works only on Chrome browser. <br/>
 
 ## 🛠️Tech Stack
+
 ![HTML5](https://img.shields.io/badge/-HTML5-E34F26?logo=html5&logoColor=white&style=flat) ![CSS3](https://img.shields.io/badge/-CSS3-1572B6?logo=css3&logoColor=white&style=flat) ![JavaScript](https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat) ![JSON](https://img.shields.io/badge/-JSON-000000?logo=json&logoColor=white&style=flat)
 
 ## ⚙️How it works?
--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js"
-import {
-    getDatabase,
-    ref,
-    push,
-    onValue,
-    remove } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-detabase.js"
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-// https://leads-tracker-app-239f0-default-rtdb.europe-west1.firebasedatabase.app/ DETABASE VALUE
-
-const firebaseConfig = {
-    databaseURL:"https://leads-tracker-app-239f0-default-rtdb.europe-west1.firebasedatabase.app/"
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const referenceInDB = ref(database, "leads");
-
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 function render(leads) {
-    let listItems = "";
+    let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
@@ -33,24 +30,18 @@ function render(leads) {
             </li>
         `
     }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems
 }
 
-onValue(referenceInDB, function(snapshot) {
-    const snapshotDoesExist = snapshot.exists()
-    if (snapshotDoesExist) {
-        const snapshotValues = snapshot.val()
-        const leads = Object.values(snapshotValues)
-        render(leads);
-    }
-})
-
 deleteBtn.addEventListener("dblclick", function() {
-    remove(referenceInDB)
-    ulEl.innerHTML = "";
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
-    push(referenceInDB, inputEl.value)
-    inputEl.value = "";
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
 })
